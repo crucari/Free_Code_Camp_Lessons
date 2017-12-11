b41455d0170d41b4d73c45539b652af5161c0a47
Carina Cruz <lecruz@bu.edu>
2017-12-11 04:41:36 +0000

    Reverse A String

---
+++ reverseAString.js
@@ -0,0 +1,18 @@
+
+function reverseString(str) {
+  var strArray = [];
+  strArray = str.split("");
+//   console.log("splitting the string:");
+//   console.log(strArray);
+  strArray.reverse();
+//   console.log(strArray);
+  var reverseStr = strArray.join("");
+//   console.log("after join");
+//   console.log(reverseStr);
+
+  return reverseStr;
+  
+ 
+}
+
+console.log(reverseString("Carina"));
